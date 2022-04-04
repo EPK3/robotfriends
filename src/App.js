49c249b40c9a5525a5e7e15a8aae9fec23346a4f
epk3 <ekrajewski3@protonmail.com>
2022-04-04 18:50:02 -0400

    modify searchbox

--- src/App.js
+++ src/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
+import { robots } from './robots';
 import SearchBox from './SearchBox';
 
-const App = () => {
-    return (
-        <div className = 'tc'>
-            <h1>RobotFriends</h1>
-            <SearchBox />
-            <CardList />
-        </div>
-        
-    );
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })   
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robots =>{
+            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        return (
+            <div className = 'tc'>
+                <h1>RobotFriends</h1>
+                <SearchBox />
+                <CardList robots={filteredRobots} />
+            </div>
+            
+        );
+    }
 }
 
 export default App;
